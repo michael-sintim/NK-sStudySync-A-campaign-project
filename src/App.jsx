a31609360c7af250ca00ca6b1a56eb96f41b7437
Michael Sintim <michaelsintim673@gmail.com>
2026-03-24 16:18:38 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import NKsStudySync from "./NKsStudySync";
 import ContactPage from "./ContactPage";
+
+// Wrapper for Home page
+function HomeWrapper() {
+  const navigate = useNavigate();
+  return <NKsStudySync onContact={() => navigate("/contact")} />;
+}
+
+// Wrapper for Contact page
+function ContactWrapper() {
+  const navigate = useNavigate();
+  return <ContactPage onBack={() => navigate("/")} />;
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<NKsStudySync />} />
-        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/" element={<HomeWrapper />} />
+        <Route path="/contact" element={<ContactWrapper />} />
       </Routes>
     </BrowserRouter>
   );
